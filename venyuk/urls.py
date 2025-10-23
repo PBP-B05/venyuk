e8bdb6b233092385c352d20c7c552d7e4950f79b
Cyrillo Praditya Soeharto <cyrillo.praditya41@ui.ac.id>
2025-10-24 00:18:40 +0700

    urls.py

--- venyuk/urls.py
+++ venyuk/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ven_shop/', include('ven_shop.urls')),
-]
+    path('promo/', include('promo.urls')),
+    path('', include('venue.urls')),
+    path('authenticate/', include('authenticate.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
